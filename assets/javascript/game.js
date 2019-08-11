9ec33d6b61dd87f6220388f8c1d45d7c978df60e
Tommy Vinyard <tpvinyard@gmail.com>
2019-08-11 13:58:38 -0500

    adding first character selection action and setting correct attributes of the new characters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     let healthPoints = "";
     let counterAttackPower = "";
     let isCharacterChosen = false;
+    let isEnemyChosen = false;
     let isGameOver = false;
 
     function resetGame () {
@@ -16,13 +17,36 @@ $(document).ready(function() {
     $(".card").on("click", function () {
 
         if (!isCharacterChosen) {
+
             $("#character-1").hide();
             $("#character-2").hide();
             $("#character-3").hide();
             $("#character-4").hide();
+
             let mainCharacter = $("<div>");
             mainCharacter.addClass("card");
+            mainCharacter.attr("data-hp", $(this).attr("data-hp"));
+            mainCharacter.attr("data-ap", $(this).attr("data-ap"));
             mainCharacter.text($(this).attr("data-hp"));
+            $("#your-character").append(mainCharacter);
+
+            $(".card").each(function() {
+                if (mainCharacter.text() !== $(this).attr("data-hp")) {
+                    let otherCharacter = $("<div>");
+                    otherCharacter.addClass("card");
+                    otherCharacter.attr("data-hp", $(this).attr("data-hp"));
+                    otherCharacter.attr("data-ap", $(this).attr("data-ap"));
+                    otherCharacter.text($(this).attr("data-hp"));
+                    $("#available-attackers").append(otherCharacter);
+                }
+            });
+            
+            isCharacterChosen = true;
+
+        } else if (!isEnemyChosen) {
+            isEnemyChosen = true;
         }
+
+
     })
 });

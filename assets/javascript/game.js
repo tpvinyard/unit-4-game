3b812572424772ab777f7afe81f719c9a5ac431a
Tommy Vinyard <tpvinyard@gmail.com>
2019-08-11 12:55:07 -0500

    adding cards that will act as characters and hiding of cards when first clicked

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,28 @@
 $(document).ready(function() {
+    let attackPower = "";
+    let healthPoints = "";
+    let counterAttackPower = "";
+    let isCharacterChosen = false;
+    let isGameOver = false;
 
+    function resetGame () {
+
+    };
+
+    function attack () {
+
+    };
+
+    $(".card").on("click", function () {
+
+        if (!isCharacterChosen) {
+            $("#character-1").hide();
+            $("#character-2").hide();
+            $("#character-3").hide();
+            $("#character-4").hide();
+            let mainCharacter = $("<div>");
+            mainCharacter.addClass("card");
+            mainCharacter.text($(this).attr("data-hp"));
+        }
+    })
 });
